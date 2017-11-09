483ffd27d849976db7535299e55888704392cd3c
Katie Maxwell <katie@codeinstitute.net>
2017-11-09 15:36:32 +0000

    table in admin

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import shoppingItem
 
 # Register your models here.
+admin.site.register(shoppingItem)
--- list/models.py
+++ list/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class shoppingItem(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    quantity = models.IntegerField(blank=False)
+    
+    def __str__(self):
+        return self.name
